bad20537f056cebb6312458313c2885a59962a41
vayne <ahaqwjwk@163.com>
2018-06-22 15:10:44 +0800

    逻辑变动

--- allpay-web/src/main/java/cn/vayne/web/service/OrderService.java
+++ allpay-web/src/main/java/cn/vayne/web/service/OrderService.java
@@ -6,6 +6,7 @@ import cn.vayne.web.domain.saasshop.ShopDO;
 import cn.vayne.web.repositorys.sass.OrderRepository;
 import cn.vayne.web.repositorys.sassshop.ShopRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -18,6 +19,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @Author: WangKun
@@ -46,14 +48,17 @@ public class OrderService {
 		}
 		try {
 			if(entities.size() > 0) {
-				Sheet sheet = workbook.createSheet("测试数据统计表");
+				Sheet sheet = workbook.createSheet("订单统计表");
 				sheet.setDefaultColumnWidth((short) 20);
+				log.info("门店:{}","ok");
+				int a = 10 / 0;
 				// 生成第一层标题样式
 				CellStyle style = getHeadStyle(workbook);
 				//正文样式
 				CellStyle styleTable = getContentStyle(workbook);
 				Row titleRow = sheet.createRow(sheet.getLastRowNum());
-				String[] column = { "订单编号", "门店CRM","门店名称","省份","城市","门店地址","订单创建日期","订单支付日期	","订单核销日期","消费者姓名","消费者联系方式","价格","数量","支付金额","支付方式","订单状态","安装状态","是否有赠品","订单来源","消费者备注"};
+				// TODO  用户id
+				String[] column = { "订单编号","用户昵称","门店CRM","门店名称","省份","城市","门店地址","订单创建日期","订单支付日期	","订单核销日期","消费者姓名","消费者联系方式","价格","数量","支付金额","支付方式","订单状态","安装状态","是否有赠品","订单来源","消费者备注"};
 				Cell cellTitle = null;
 				for (int i = 0; i < column.length; i++) {
 					cellTitle = titleRow.createCell(i);
@@ -62,21 +67,178 @@ public class OrderService {
 				}
 				Cell cellTable = null;
 				for (OrderInfo entity : entities) {
+					Long shopId = entity.getShopId();
+					ShopDO shopDO = null;
+					if(shopId != null) {
+						Set<String> shopIds = shopMaps.keySet();
+						if(shopIds.contains(shopId)) {
+							shopDO = shopMaps.get(shopId);
+						}else {
+							shopDO = new ShopDO();
+						}
+					}else {
+						shopDO = new ShopDO();
+					}
 					Row dataRow = sheet.createRow(sheet.getLastRowNum() + 1);
-					cellTable = dataRow.createCell(0);
-					cellTable.setCellValue(entity.getId());
-					cellTable.setCellStyle(styleTable);
-
-					cellTable = dataRow.createCell(1);
+					cellTable = dataRow.createCell(0);//订单编号
 					cellTable.setCellValue(entity.getOrderNo());
 					cellTable.setCellStyle(styleTable);
 
-					cellTable = dataRow.createCell(2);
-					cellTable.setCellValue(entity.getOrderStatus());
+					cellTable = dataRow.createCell(1);//用户昵称
+					cellTable.setCellValue(entity.getUserId());
 					cellTable.setCellStyle(styleTable);
 
-					cellTable = dataRow.createCell(3);
-					cellTable.setCellValue(entity.getPaymentType());
+
+
+					cellTable = dataRow.createCell(2);//门店CRM
+					cellTable.setCellValue(shopDO.getCrm() == null ? "" : shopDO.getCrm());
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(3);//门店名称
+					cellTable.setCellValue(shopDO.getName() == null ? "" : shopDO.getName());
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(4);//省份
+					cellTable.setCellValue(shopDO.getProvince() == null ? "" : shopDO.getProvince());
+					cellTable.setCellStyle(styleTable);
+
+
+					cellTable = dataRow.createCell(5);//城市
+					cellTable.setCellValue(shopDO.getCity() == null ? "" : shopDO.getCity());
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(6);//门店地址
+					cellTable.setCellValue(shopDO.getAddress() == null ? "" : shopDO.getAddress());
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(7);//订单创建日期
+					cellTable.setCellValue(entity.getCreatedTime());
+					cellTable.setCellStyle(styleTable);
+
+					// TODO 支付时间字段待确认
+					cellTable = dataRow.createCell(8);//订单支付日期
+					cellTable.setCellValue(entity.getCreatedTime());
+					cellTable.setCellStyle(styleTable);
+
+
+					cellTable = dataRow.createCell(9);//订单核销日期
+					cellTable.setCellValue(entity.getFinishTime());
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(10);//消费者姓名
+					cellTable.setCellValue(entity.getReceiverUserName());
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(11);//消费者联系方式
+					cellTable.setCellValue(entity.getReceiverMobile());
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(12);//价格
+					cellTable.setCellValue(entity.getItemAmount());
+					cellTable.setCellStyle(styleTable);
+
+					//TODO 数量待定
+					cellTable = dataRow.createCell(13);//数量
+					cellTable.setCellValue(entity.getFinishTime());
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(14);//支付金额
+					cellTable.setCellValue(entity.getActualPayment());
+					cellTable.setCellStyle(styleTable);
+
+					String paymentType = entity.getPaymentType();
+					if(StringUtils.isNotBlank(paymentType)) {
+						switch (paymentType) {
+							case "OFFLINE" :
+								paymentType = "线下/到店支付";
+								break;
+							case "ONLINE" :
+								paymentType = "线上/在线支付";
+								break;
+							default:
+								paymentType = "";
+						}
+					}
+					cellTable = dataRow.createCell(15);//支付方式
+					cellTable.setCellValue(paymentType);
+					cellTable.setCellStyle(styleTable);
+
+					String orderStatus = entity.getOrderStatus();
+					if(StringUtils.isNotBlank(orderStatus)) {
+						switch (orderStatus) {
+							case "UNPAID":
+								orderStatus = "待支付";
+								break;
+							case "PAID":
+								orderStatus = "已支付";
+								break;
+							case "FINISHED":
+								orderStatus = "已完成";
+								break;
+							case "CANCEL":
+								orderStatus = "已取消";
+								break;
+							default:
+								orderStatus = "";
+						}
+					}
+
+					cellTable = dataRow.createCell(16);//订单状态
+					cellTable.setCellValue(orderStatus);
+					cellTable.setCellStyle(styleTable);
+
+					String serviceStatus = entity.getServiceStatus();
+					if(StringUtils.isNotBlank(serviceStatus)) {
+						switch (serviceStatus) {
+							case "SEND_BILL":
+								serviceStatus = "派单中";
+								break;
+							case "BE_SEND_BILL":
+								serviceStatus = "待派单";
+								break;
+							case "BE_RESERVED":
+								serviceStatus = "待预约";
+								break;
+							case "BE_INSTALLED":
+								serviceStatus = "待安装";
+								break;
+							case "INSTALLING":
+								serviceStatus = "安装完成";
+								break;
+							case "CANCEL":
+								serviceStatus = "取消";
+								break;
+							default:
+								serviceStatus = "";
+						}
+					}
+					cellTable = dataRow.createCell(17);//安装状态
+					cellTable.setCellValue(serviceStatus);
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(18);//是否有赠品
+					cellTable.setCellValue(entity.getIsGift().equals("Y") ? "是" : "否");
+					cellTable.setCellStyle(styleTable);
+
+					String sourceType = entity.getSourceType();
+					if(StringUtils.isNotBlank(sourceType)) {
+						switch (sourceType) {
+							case "IOS_VIEW":
+								sourceType = "IOS微信小程序";
+								break;
+							case "ANDROID_VIEW":
+								sourceType = "安卓微信小程序";
+								break;
+							default:
+								sourceType = "";
+						}
+					}
+					cellTable = dataRow.createCell(19);//订单来源
+					cellTable.setCellValue(sourceType);
+					cellTable.setCellStyle(styleTable);
+
+					cellTable = dataRow.createCell(20);//消费者备注
+					cellTable.setCellValue(entity.getRemark());
 					cellTable.setCellStyle(styleTable);
 				}
 			}else {
