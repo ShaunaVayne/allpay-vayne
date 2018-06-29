b64b26709a0fec63d0c4ff9386ca7bd769ec7673
wangkun <wangkun@tuhu.cn>
2018-06-29 10:56:34 +0800

    更改controller层代码

--- allpay-web/src/main/java/cn/vayne/web/controller/HelloController.java
+++ allpay-web/src/main/java/cn/vayne/web/controller/HelloController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.BASE64Encoder;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.FileOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.Future;
@@ -33,9 +35,28 @@ public class HelloController {
 	@Autowired
 	private ExcelPoiService excelPoiService;
 
-	@RequestMapping("/hello/{id}")
-	public String submit(HttpServletRequest httpServletRequest, @PathVariable("id") String id) throws Exception {
-		switch (id) {
+	@RequestMapping("/hello")
+	public String submit(ExcelPoiReq req, HttpServletResponse response, HttpServletRequest request) throws Exception {
+		try {
+			Future<HSSFWorkbook> workbookFuture = excelPoiService.excelOut(req,"test");
+			while(true) {
+				if(workbookFuture.isDone()) {
+					String dateTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+					response.setContentType("application/x-excel;charset=UTF-8");
+					String filename = "订单统计表"+ dateTime +".xls";
+					String agent = request.getHeader("user-agent");
+					filename = encodeDownloadFilename(filename, agent);
+					response.setHeader("Content-Disposition", "attachment;filename="
+							+ filename);
+					workbookFuture.get().write(response.getOutputStream());
+					break;
+				}
+			}
+		}catch(Exception e) {
+			log.warn("查询数据进行导出数据异常:{}", e);
+
+		}
+		/*switch (id) {
 			case "1" :
 				id = "chrome";
 				break;
@@ -58,12 +79,17 @@ public class HelloController {
 		String dateTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 		String filename = "/Users/vayne/myself/tip/data/"+"订单统计表"+ id + dateTime +".xls";
 		FileOutputStream os = new FileOutputStream(filename);
-		workbookFuture.get().write(os);
-		os.flush();
-		os.close();
+		while (true) {
+			if(workbookFuture.isDone()) {
+				workbookFuture.get().write(os);
+				os.flush();
+				os.close();
+				break;
+			}
+		}
 		long endTime = System.currentTimeMillis();
 		log.info("报表导出执行时间{}",endTime - startTime);
-		log.info("end submit");
+		log.info("end submit");*/
 		return "success";
 	}
 
@@ -71,5 +97,14 @@ public class HelloController {
 	public Object test2(HttpServletRequest request, @PathVariable("id") String id) {
 		log.info("参数:{}", id);
 		return request.getHeader("User-Agent");
+	}
+
+	public static String encodeDownloadFilename(String filename, String agent)
+			throws IOException {
+		filename = "=?gbk?B?"
+				+ new BASE64Encoder().encode(filename.getBytes("gbk"))
+				+ "?=";
+		filename = filename.replaceAll("\r\n", "");
+		return filename;
 	}
 }
