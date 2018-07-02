8fa8bda3a75fd839719a5aab97c5bec509c73190
wangkun <wangkun@tuhu.cn>
2018-07-02 11:18:19 +0800

    CountDownLatch类的用法

--- allpay-web/src/test/java/cn/vayne/test/demo/Main.java
+++ allpay-web/src/test/java/cn/vayne/test/demo/Main.java
@@ -1,6 +1,5 @@
 package cn.vayne.test.demo;
 
-import cn.vayne.test.BaseTest;
 import cn.vayne.web.config.ApplicationContextProvider;
 import cn.vayne.web.domain.DTO.ExcelPoiReq;
 import cn.vayne.web.service.ExcelPoiService;
@@ -10,6 +9,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * @Author: WangKun
@@ -19,7 +19,7 @@ import javax.annotation.Resource;
  * @Version: 1.0.0
  */
 @Slf4j
-public class Main extends BaseTest {
+public class Main {
 
 	@Autowired
 	private ExcelPoiService excelPoiService;
@@ -41,6 +41,41 @@ public class Main extends BaseTest {
 		m4.getExcelPoiService().excelOut(req,"4d");
 		m5.getExcelPoiService().excelOut(req,"5e");
 		log.info("end");
-
 	}
+
+	@Test
+	public void test() {
+		final CountDownLatch latch = new CountDownLatch(2);
+		new Thread(() -> {
+			try {
+				System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+				Thread.sleep(3000);
+				System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+				latch.countDown();
+			}catch(Exception e) {
+			    log.error("");
+			}
+		}).start();
+
+		new Thread(() -> {
+			try {
+				System.out.println("子线程"+Thread.currentThread().getName()+"正在执行");
+				Thread.sleep(3000);
+				System.out.println("子线程"+Thread.currentThread().getName()+"执行完毕");
+				latch.countDown();
+			}catch(Exception e) {
+			    log.error("");
+			}
+		}).start();
+
+		try {
+			System.out.println("等待2个子线程执行完毕...");
+			latch.await();	//调用await()方法的线程会被挂起，它会等待直到count值为0才继续执行
+			System.out.println("2个子线程已经执行完毕");
+			System.out.println("继续执行主线程");
+		}catch(Exception e) {
+		    log.error("");
+		}
+	}
+
 }
