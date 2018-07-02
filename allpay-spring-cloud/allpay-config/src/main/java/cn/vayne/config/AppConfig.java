78941a433128a1911656c7a94c347d819339e89b
wangkun <wangkun@tuhu.cn>
2018-07-02 17:32:01 +0800

    远程连接配置文件搭建

--- allpay-spring-cloud/allpay-config/src/main/java/cn/vayne/config/AppConfig.java
+++ allpay-spring-cloud/allpay-config/src/main/java/cn/vayne/config/AppConfig.java
@@ -2,6 +2,7 @@ package cn.vayne.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -13,7 +14,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class AppConfig {
 
 	public static void main(String[] args) {
