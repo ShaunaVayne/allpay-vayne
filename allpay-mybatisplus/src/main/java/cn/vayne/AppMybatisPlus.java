9f05d9a2237d5cbc939a1fd80a3017021d395233
王琨 <wangkun@tuhu.cn>
2019-07-01 20:41:26 +0800

    mybatisPlus

---
+++ allpay-mybatisplus/src/main/java/cn/vayne/AppMybatisPlus.java
@@ -0,0 +1,19 @@
+package cn.vayne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: WangKun
+ * @Description:
+ * @Date: Created in 2019/4/25 4:20 PM
+ * @ProjectName: allpay-vayne
+ * @Version: 1.0.0
+ */
+@SpringBootApplication
+public class AppMybatisPlus {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMybatisPlus.class, args);
+	}
+}
